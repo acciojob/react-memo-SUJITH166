292eb1ca1b5340ef084908e912a30dbf979cf938
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-02-02 23:53:48 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const ReactMemo = React.memo(({ task }) => {
-  console.log('Rendering task:', task);
-  return <li>{task}</li>;
-});
+const ReactMemo = () => {
+  const [input, setInput] = useState("");
 
-export default ReactMemo;
+  const [list, setList] = useState([]);
+
+  const onInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+  const onAddSkill = () => {
+    if (input.trim().length > 5) {
+      setList((prev) => [...prev, input.trim()]);
+      setInput("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>React.memo</h1>
+      <div>
+        <input
+          value={input}
+          onChange={onInput}
+          placeholder="Enter a custom task"
+        />
+        <button onClick={onAddSkill}>Add Skill</button>
+      </div>
+      <ul>
+        {list.map((i, index) => (
+          <li key={index}>{i}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(ReactMemo);
